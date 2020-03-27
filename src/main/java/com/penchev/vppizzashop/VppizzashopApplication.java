28389a4afb4ba2dfbfa565741a33b48e26ce36f7
Vladislav <vvld964@gmail.com>
2020-03-27 12:31:08 +0200

    add Index page

---
+++ src/main/java/com/penchev/vppizzashop/VppizzashopApplication.java
@@ -0,0 +1,13 @@
+package com.penchev.vppizzashop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VppizzashopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VppizzashopApplication.class, args);
+    }
+
+}
